943782b8d5812b0f7b5349ce75530464b6344943
utrodus <come.programmer@gmail.com>
2021-04-28 08:12:46 +0700

    add route to edit note by id

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const {addNoteHandler, getAllNotesHandler, getNoteByIdHandler} = require("./handler");
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require("./handler");
 
 const routes = [
   {
@@ -15,6 +15,11 @@ const routes = [
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler
   },
 ];
 
