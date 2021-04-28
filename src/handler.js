abe7d51091fd0b61912050f8063cbcc8efcdd48a
utrodus <come.programmer@gmail.com>
2021-04-28 08:12:56 +0700

    create handler to edit note by id

--- src/handler.js
+++ src/handler.js
@@ -71,8 +71,39 @@ const getNoteByIdHandler = (request, h) => {
   response.code(404);
   return response;
 }
+editNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const { title, tags, body } = request.payload;
+  const updatedAt = new Date().toISOString();
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+}
 
 module.exports = {
   addNoteHandler, getAllNotesHandler,
-  getNoteByIdHandler
+  getNoteByIdHandler,
+  editNoteByIdHandler,
 };
